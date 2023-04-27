6ff89a960d2153582be3495751cc029ec33a8132
tuvd <vodinhtu28@gmail.com>
2023-04-27 22:35:24 +0700

    login admin

--- app/controllers/login.controller.js
+++ app/controllers/login.controller.js
@@ -69,9 +69,63 @@ exports.login = async (req, res) => {
             });
         }
     } else {
-        return res.status(400).send({
-            message: "Invalid page parameter."
-        });
+        try {
+            // Find account
+            const data = await Admin.findOne({ email: req.body.email });
+
+            if (!data) {
+                return res.status(401).send({
+                    message: `Admin not found with email ${req.body.email}.`
+                });
+            }
+
+            // Check password
+            const isPasswordValid = bcrypt.compareSync(req.body.password, data.hash_password);
+            if (!isPasswordValid) {
+                return res.status(401).send({
+                    message: `Incorrect password.`
+                });
+            }
+
+            const accessTokenLife = process.env.ACCESS_TOKEN_LIFE || jwtVariable.accessTokenLife;
+            const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || jwtVariable.accessTokenSecret;
+            const dataForAccessToken = {
+                id: data.id,
+                email: data.email,
+                firstName: data.first_name,
+                lastName: data.last_name,
+                phone: data.phone,
+                age: data.age,
+                gender: data.gender,
+                avatar: data.avatar,
+                role: data.role
+            };
+            const accessToken = await authMethod.generateToken(
+                dataForAccessToken,
+                accessTokenSecret,
+                accessTokenLife,
+            );
+            if (!accessToken) {
+                return res.status(500).send({
+                    message: `Create token failed.`
+                });
+            }
+
+            const refreshTokenLife = process.env.ACCESS_TOKEN_LIFE || jwtVariable.refreshTokenLife;
+            const refreshTokenSecret = process.env.ACCESS_TOKEN_SECRET || jwtVariable.refreshTokenSecret;
+            const refreshToken = await authMethod.generateToken(
+                dataForAccessToken,
+                refreshTokenSecret,
+                refreshTokenLife,
+            );
+
+            return res.json({ accessToken, refreshToken });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send({
+                message: "An error occurred while processing your request."
+            });
+        }
     }
 };
 
@@ -125,6 +179,10 @@ exports.register = async (req, res) => {
 
 exports.refreshToken = async (req, res) => {
     try {
+        if (!req.body.page) {
+            return res.status(400).send({ message: "Content can not be empty!" });
+        }
+
         // Get refresh token from body
         const refreshTokenFromBody = req.body.refreshToken;
         if (!refreshTokenFromBody) {
@@ -139,27 +197,53 @@ exports.refreshToken = async (req, res) => {
         }
 
         const email = decodedRefresh.payload.email;
-        const customer = await Customer.findOne({ email });
-        if (!customer) {
-            return res.status(401).send({
-                message: `Customer not found with email ${email}.`,
-            });
+        var customer;
+        var admin;
+        if (req.body.page === 'user') {
+            customer = await Customer.findOne({ email });
+            if (!customer) {
+                return res.status(401).send({
+                    message: `Customer not found with email ${email}.`,
+                });
+            }
+        } else {
+            admin = await Admin.findOne({ email });
+            if (!admin) {
+                return res.status(401).send({
+                    message: `Admin not found with email ${email}.`,
+                });
+            }
         }
 
         const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || jwtVariable.accessTokenSecret;
         const accessTokenLife = process.env.ACCESS_TOKEN_LIFE || jwtVariable.accessTokenLife;
 
-        // Create new access token
-        const dataForAccessToken = {
-            id: customer.id,
-            email: customer.email,
-            firstName: customer.first_name,
-            lastName: customer.last_name,
-            phone: customer.phone,
-            age: customer.age,
-            gender: customer.gender,
-            avatar: customer.avatar,
-        };
+        var dataForAccessToken;
+        if (customer) {
+            // Create new access token
+            dataForAccessToken = {
+                id: customer.id,
+                email: customer.email,
+                firstName: customer.first_name,
+                lastName: customer.last_name,
+                phone: customer.phone,
+                age: customer.age,
+                gender: customer.gender,
+                avatar: customer.avatar,
+            };
+        } else {
+            dataForAccessToken = {
+                id: admin.id,
+                email: admin.email,
+                firstName: admin.first_name,
+                lastName: admin.last_name,
+                phone: admin.phone,
+                age: admin.age,
+                gender: admin.gender,
+                avatar: admin.avatar,
+                role: admin.role,
+            };
+        }
 
         const accessToken = await authMethod.generateToken(dataForAccessToken, accessTokenSecret, accessTokenLife);
         if (!accessToken) {
